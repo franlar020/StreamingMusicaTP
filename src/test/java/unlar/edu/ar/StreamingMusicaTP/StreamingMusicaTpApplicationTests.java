249482d5312979ed666f7533d483c9f2c60266a7
Francisco Antonio Gonzalez <franlar020@gmail.com>
2026-04-25 12:24:28 -0300

    Agregar archivos de configuración y estructura inicial del proyecto Spring Boot

---
+++ src/test/java/unlar/edu/ar/StreamingMusicaTP/StreamingMusicaTpApplicationTests.java
@@ -0,0 +1,13 @@
+package unlar.edu.ar.StreamingMusicaTP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamingMusicaTpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
